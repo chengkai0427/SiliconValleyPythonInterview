40728c393db2448e55188789f10d0d291932407f
chengkai <502616487@qq.com>
2025-11-25 19:26:47 +0800

    查找两个链表的交点
    先构建两个单链表，然后将其中一个单链表的尾节点指向另一个单链表的随机节点，最后找出找两个单链表的交点。

--- get_intersect_node.py
+++ get_intersect_node.py
@@ -1,19 +1,24 @@
 '''先构建两个单链表，然后将其中一个单链表的尾节点指向另一个单链表的随机节点，最后找出找两个单链表的交点'''
 import random
 from chapter07eg01cyclelink import LinkedList
+# 创建两个单链表
 llist1 = LinkedList()
 llist2 = LinkedList()
 for i in range(random.randint(1,15)):
     llist1.append(i)
 for i in range(random.randint(1,10)):
     llist2.append(i)
+# 将第二个单链表的尾节点指向第一个单链表的随机节点
 inter_node = llist1.head
-for i in range(random.randint(1,10)):
+N=0
+while N<random.randint(1,10) and inter_node:
     inter_node = inter_node.next
+    N+=1
 tail = llist2.head
 while tail.next:
     tail = tail.next
 tail.next = inter_node
+# 通过将一个链表的节点放入set集合，然后遍历另一个链表的节点，如果节点在集合中，则找到两个单链表的交点
 list1_node_set = set()
 head1 = llist1.head
 while head1:
